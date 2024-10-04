c8afd47d19ad45e2c2c8d9157e27bbda0da51505
Kamicarze73 <andricarzy.03@gmail.com>
2024-10-04 18:04:39 +0200

    Metà folgio 3

--- Sheet3/src/main.rs
+++ Sheet3/src/main.rs
@@ -1,4 +1,114 @@
 use std::collections::HashMap;
+use std::fmt;
+use std::fmt::{write, Formatter};
+use crate::Item::{Chips, Coffee, Coke};
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone)]
+enum Item {
+    Coke,
+    Coffee,
+    Chips,
+}
+
+enum Coin {
+    TenCents,
+    TwentyCents,
+    FiftyCents,
+    Euro1,
+    Euro2,
+}
+
+impl Coin {
+    fn to_cents(&self) -> u32 {
+        match self {
+            Coin::TenCents => 10,
+            Coin::TwentyCents => 20,
+            Coin::FiftyCents => 50,
+            Coin::Euro1 => 100,
+            Coin::Euro2 => 200,
+        }
+    }
+}
+
+struct VendingMachine {
+    coins: u32, //soldi all'interno
+    items: HashMap<Item, usize>, //item e quantita disponibili
+}
+
+impl VendingMachine {
+    fn new(mut items: HashMap<Item, usize>) -> Self {
+        Self { coins: 0, items }
+    }
+
+    fn add_item(&mut self, item: Item, quantity: usize) {
+        self.items.insert(item, quantity);
+    }
+
+    fn insert_coin(&mut self, coin: Coin) -> Result<&str, &str> {
+        let result = match coin {
+            Coin::TenCents => Ok("10 Cent inserted"),
+            Coin::TwentyCents => Ok("20 Cent inserted"),
+            Coin::FiftyCents => Ok("50 Cent inserted"),
+            Coin::Euro1 => Ok("1 Euro inserted"),
+            Coin::Euro2 => Ok("2 Euro inserted"),
+            _ => Err("Invalid coin inserted"),
+        };
+        if result.is_ok() {
+            self.coins += coin.to_cents();
+        }
+        result
+    }
+
+    fn get_item_price(item: &Item) -> u32 {
+        match item {
+            Item::Coke => 350,
+            Chips => 200,
+            Item::Coffee => 150
+        }
+    }
+
+    fn buy(&mut self, item: Item) -> Result<u32, &str> {
+        let item_price = VendingMachine::get_item_price(&item);
+        let quantity_item = self.items.get_mut(&item).unwrap();
+        if self.coins >= item_price && *quantity_item as u32 > 0 {
+            let resto = self.coins - item_price;
+            self.coins -= item_price;
+            *quantity_item -= 1;
+            Ok(resto)
+        } else {
+            Err("Credito non sufficente o credito non sufficente")
+        }
+    }
+}
+#[derive(Debug)]
+struct Date(u8, u8, u16);
+#[derive(Debug)]
+struct Hour(u8, u8);
+#[derive(Debug)]
+struct BoxShipping {
+    name: String,
+    barecode: String,
+    shipment_date: Date,
+    shipment_hour: Hour,
+}
+
+impl fmt::Display for Date {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}/{:02}/{:04}", self.0, self.1, self.2)
+    }
+}
+
+impl fmt::Display for Hour {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.0, self.1)
+    }
+}
+
+impl fmt::Display for BoxShipping {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f,"name: {}", self.name)
+    }
+}
 
 fn main() {
     //ES1
@@ -11,13 +121,30 @@ fn main() {
     Esempio 3: Struct
      */
 
-    //ES3
+    //ES3 non come lo vuole lui
     let mut hm: HashMap<&str, &str> = HashMap::new();
     hm.insert("123DJ123", "Andrea");
     hm.insert("HDGKLK23", "Luca");
     hm.insert("LOOOL123", "Marco");
     println!("Owner of HDGKLK23: {}", recognise_owner(&hm, "HDGKLK23").unwrap());
     println!("Owner of AAAAAA: {}", recognise_owner(&hm, "AAAAAAA").unwrap_or(String::from("Non trovato")));
+
+    //ES4
+    let mut hash_map: HashMap<Item, usize> = HashMap::new();
+    hash_map.insert(Chips, 40);
+    hash_map.insert(Coffee, 0);
+    hash_map.insert(Coke, 2);
+    let mut vending_machine = VendingMachine::new(hash_map);
+    vending_machine.add_item(Chips, 20);
+    println!("{}", vending_machine.insert_coin(Coin::Euro1).unwrap());
+    println!("{}", vending_machine.insert_coin(Coin::Euro2).unwrap());
+    //println!("{}",vending_machine.buy(Coke).unwrap());
+    //println!("{}",vending_machine.buy(Coffee).unwrap());
+    println!("{}", vending_machine.buy(Chips).unwrap());
+
+    //ES5
+
+    //ES6
 }
 
 fn recognise_owner(hm: &HashMap<&str, &str>, plate: &str) -> Option<String> {
--- Sheet3/target/debug/deps/Sheet3.d
+++ Sheet3/target/debug/deps/Sheet3.d
@@ -1,5 +1,5 @@
-C:\Users\Carzeri\Desktop\Rust-Exercise\Sheet3\target\debug\deps\Sheet3.exe: src/main.rs
+C:\Users\andri\Desktop\Rust-Exercise\Sheet3\target\debug\deps\Sheet3.exe: src/main.rs
 
-C:\Users\Carzeri\Desktop\Rust-Exercise\Sheet3\target\debug\deps\Sheet3.d: src/main.rs
+C:\Users\andri\Desktop\Rust-Exercise\Sheet3\target\debug\deps\Sheet3.d: src/main.rs
 
 src/main.rs:
