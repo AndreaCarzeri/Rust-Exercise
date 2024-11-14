10db5e60e5cefc0628f473377fad439de5b382d1
Kamicarze73 <andricarzy.03@gmail.com>
2024-11-14 16:31:37 +0100

    sheet 4 completed

--- Sheet4/src/main.rs
+++ Sheet4/src/main.rs
@@ -1,19 +1,21 @@
-use std::collections::LinkedList;
+use std::collections::{HashMap, LinkedList};
 use std::f32::consts::PI;
 use std::fmt::{write, Debug, Display, Formatter};
 use std::ops::{Add, Sub};
 use rand::Rng;
+use crate::Permission::{EXECUTE, READ, WRITE};
+use crate::Role::{ADMIN, GUEST, USER};
 
 fn main() {
     println!("{:?}", find_equal("lollo", "alelo"));
     println!("{:?}", lucky_slice("lollololololandreaalebusola"));
 
     //ES2
-    let p1 = Person::new("Raffaele".to_string(), None, None);
+    let p1 = Person::new("Genoveffo".to_string(), None, None);
     let p2 = Person::new("Adelaide".to_string(), None, None);
-    let p3 = Person::new("Renata".to_string(), None, None);
-    let p4 = Person::new("Barbara".to_string(), Some(&p1), Some(&p2));
-    let p5 = Person::new("Michele".to_string(), None, Some(&p3));
+    let p3 = Person::new("Luigia".to_string(), None, None);
+    let p4 = Person::new("Rapuano".to_string(), Some(&p1), Some(&p2));
+    let p5 = Person::new("Vincenzo".to_string(), None, Some(&p3));
     let p6 = Person::new("Andrea".to_string(), Some(&p5), Some(&p4));
     println!("Gen 2 from andrea: {:?}", p6.find_relatives(2));
     println!("Roots from andrea: {:?}", p6.find_roots());
@@ -26,6 +28,195 @@ fn main() {
     lista.push_back(4.0);
     lista.push_back(5.0);
     println!("Lista divisa {:?}", lista.split());
+    //ES7
+    let telephone_number = "+398721871827";
+    let prefix = "+37";
+    println!("Risultato telefono: {}", skip_prefix(telephone_number, prefix));
+}
+
+//ES9
+#[derive(PartialEq, Eq)]
+enum Role {
+    GUEST,
+    USER,
+    ADMIN,
+}
+
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
+enum Permission {
+    READ,
+    WRITE,
+    EXECUTE,
+}
+
+struct Actions {
+    action: String,
+    permission: HashMap<Permission, bool>,
+}
+
+struct User {
+    name: String,
+    role: Role,
+    actions: Vec<Actions>,
+}
+
+trait Auth {
+    fn check_permission(&self, action: &str, permission_type: &Permission) -> bool;
+    fn can_write(&self, string: &str) -> bool;
+    fn can_read(&self, string: &str) -> bool;
+    fn can_execute(&self, string: &str) -> bool;
+}
+
+impl Auth for User {
+    fn check_permission(&self, action: &str, permission_type: &Permission) -> bool {
+        for a in self.actions.iter() {
+            if a.action.eq(action) { //guardo se nella posizione "permission_type" è true o false
+                return a.permission[permission_type];
+            }
+        }
+        false
+    }
+
+    fn can_write(&self, string: &str) -> bool {
+        self.check_permission(string, &WRITE)
+    }
+
+    fn can_read(&self, string: &str) -> bool {
+        self.check_permission(string, &READ)
+    }
+
+    fn can_execute(&self, string: &str) -> bool {
+        self.check_permission(string, &EXECUTE)
+    }
+}
+
+impl Default for Actions {
+    fn default() -> Self {
+        let mut hm: HashMap<Permission, bool> = HashMap::new();
+        hm.insert(WRITE, false);
+        hm.insert(READ, false);
+        hm.insert(EXECUTE, false);
+        Self { action: "".to_string(), permission: hm }
+    }
+}
+
+impl Actions {
+    fn new(action: String, read: bool, write: bool, execute: bool) -> Self {
+        let mut hm: HashMap<Permission, bool> = HashMap::new();
+        hm.insert(WRITE, write);
+        hm.insert(READ, read);
+        hm.insert(EXECUTE, execute);
+        Self { action, permission: hm }
+    }
+}
+
+impl Default for User {
+    fn default() -> Self {
+        Self { name: "Guest".to_string(), role: GUEST, actions: Vec::new() }
+    }
+}
+
+impl User {
+    fn change_role(&mut self, role: Role) -> Result<(), String> {
+        if self.role == ADMIN {
+            self.role = role;
+            Ok(())
+        } else if self.role == USER {
+            if role == ADMIN {
+                Err("You can't be ADMIN".to_string())
+            } else {
+                self.role=role;
+                Ok(())
+            }
+        } else {
+            if role == GUEST {
+                self.role=role;
+                Ok(())
+            } else {
+                Err("You can't cange your role".to_string())
+            }
+        }
+    }
+}
+
+fn sudo_change_permission(user: &mut User, string: String, permission: Permission, value: bool){
+    for mut a in user.actions{
+        if a.action==string {
+            a.permission.insert(permission, value);
+        }
+    }
+}
+
+
+//ES8
+#[derive(Debug)]
+struct Chair<'a> {
+    color: &'a str,
+    quantity: &'a usize,
+}
+
+#[derive(Debug)]
+struct Wardrobe<'a> {
+    color: &'a str,
+    quantity: &'a usize,
+}
+
+trait Object {
+    fn build(&self) -> &str;
+    fn get_quantity(&self) -> String;
+}
+
+impl<'a> Object for Chair<'a> {
+    fn build(&self) -> &str {
+        "Chair has been built"
+    }
+
+    fn get_quantity(&self) -> String {
+        format!("We have {} chairs", self.quantity)
+    }
+}
+
+impl<'a> Object for Wardrobe<'a> {
+    fn build(&self) -> &str {
+        "Wardrobe has been built"
+    }
+
+    fn get_quantity(&self) -> String {
+        format!("We have {} wardrobe", self.quantity)
+    }
+}
+
+impl<'a> Display for Chair<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.quantity <= &0 {
+            write!(f, "There are no chair")
+        } else if self.quantity == &1 {
+            write!(f, "There is a {} chair", self.color)
+        } else {
+            write!(f, "There are {} {} chair", self.quantity, self.color)
+        }
+    }
+}
+
+impl<'a> Display for Wardrobe<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        if self.quantity <= &0 {
+            write!(f, "There are no wardrobe")
+        } else if self.quantity == &1 {
+            write!(f, "There is a {} wardrobe", self.color)
+        } else {
+            write!(f, "There are {} {} wardrobe", self.quantity, self.color)
+        }
+    }
+}
+
+//ES7
+fn skip_prefix<'b, 'a>(telephone_number: &'a str, prefix: &'b str) -> &'a str {
+    if !telephone_number.contains(prefix) {
+        telephone_number
+    } else {
+        telephone_number.split_at(prefix.len()).1
+    }
 }
 
 //ES6
@@ -127,11 +318,11 @@ impl Add for Area {
     }
 }
 
-impl<'a> Add for &dyn GetArea {
-    type Output = &'a dyn GetArea;
+impl Add for &dyn GetArea {
+    type Output = Area;
 
-    fn add(self, rhs: Area) -> Self::Output {
-        self.get_area() + rhs
+    fn add(self, rhs: Self) -> Self::Output {
+        self.get_area() + rhs.get_area()
     }
 }
 
